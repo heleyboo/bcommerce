c2db222c0f3cc79fd5293e7332d417c5987855f2
Hoan Do Van <vanhoan90it6@gmail.com>
2021-04-20 14:27:30 +0700

    Update  json mapping"

--- src/main/java/com/binh/core/config/ModelMapperConfig.java
+++ src/main/java/com/binh/core/config/ModelMapperConfig.java
@@ -4,6 +4,10 @@ import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 
 @Configuration
 public class ModelMapperConfig {
@@ -13,4 +17,13 @@ public class ModelMapperConfig {
         ModelMapper modelMapper = new ModelMapper();
         return modelMapper;
     }
+	
+	@Bean
+	public MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter() {
+	    ObjectMapper mapper = new ObjectMapper();
+	    mapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
+	    MappingJackson2HttpMessageConverter converter = 
+	        new MappingJackson2HttpMessageConverter(mapper);
+	    return converter;
+	}
 }
